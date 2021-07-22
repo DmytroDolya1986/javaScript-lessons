b0da3d9451d1684a785cce75c10d8eea6b759897
Dmytro Dolya <dmytrodolya1986@gmail.com>
2021-07-22 09:33:09 +0300

    Neeew commit

---
+++ Visibility_Functions/index.js
@@ -0,0 +1,67 @@
+// // Visibility_Functions
+// // 'use strict';
+
+// // const message = 'Just lern it!';
+
+// // const sendMessage = (name) => {
+// //     const sender = 'Gromcode'
+// //     console.log(`${name}, ${message}. You ${sender}`);
+// // }
+
+// // sendMessage('Tom');
+
+// // let message = 'Just lern it!';
+
+// // function sendMessage(name){
+// //     const sender = 'Gromcode'
+  
+// //     console.log(`${name}, ${message}. You ${sender}`);
+// // }
+
+// // function setMessage(text) {
+// //     message = text;
+// // }
+// // sendMessage('Tom');
+
+// // setMessage('Hello!');
+
+// // sendMessage('Tom');
+
+// // message = 'Hi';
+
+// // sendMessage('Tom');
+
+
+
+// // messager
+// let message = 'Hi!';
+
+// const createMessenger = () => {
+    
+
+//     function sendMessage(name) {
+//         const sender = 'Gromcode';
+      
+//         console.log(`${name}, ${message}. You ${sender}`);
+//     }
+    
+//     function setMessage(text) {
+//         message = text;
+//     } 
+
+//     return{
+//         sendMessage,
+//         setMessage,
+//     };
+// };
+
+// const messenger1 = createMessenger();
+
+// messenger1.sendMessage('Bob');
+
+// const messenger2 = createMessenger();
+
+// messenger2.sendMessage('Tom');
+// messenger2.setMessage('Hello');
+
+// messenger1.sendMessage('Ann');
---
+++ Visibility_Functions_Razbiraemsya/index.js
@@ -0,0 +1,44 @@
+// counter
+
+// const makeCounter = () => {
+//     let count = 0;
+
+//     return function() {
+//         return count++;
+//     };
+// };
+
+// const counter1 = makeCounter();
+// const counter2 = makeCounter();
+
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+
+// console.log(counter2());
+// console.log(counter2());
+
+            //Message
+
+
+// const message = 'Have a good day!';
+
+// function sendMessage(name) {
+//     let greeting = 'Hi, ';
+
+//     if (name) {
+//         const greeting = 'Hello, ';
+//         const name = 'Tad';
+//         console.log(greeting + name);
+
+//     } else {
+//         console.log(message);
+//     }
+// }
+
+// sendMessage(' ');
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+};
