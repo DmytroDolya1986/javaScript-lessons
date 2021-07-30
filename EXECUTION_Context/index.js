3202e4a465a8407ca36b7e3a05cee3cff5789039
Dmytro Dolya <dmytrodolya1986@gmail.com>
2021-07-30 12:09:43 +0300

    NEXT_oNE

--- EXECUTION_Context/index.js
+++ EXECUTION_Context/index.js
@@ -62,19 +62,64 @@
 
 // setTimeout(() => user.sayHi(), 100);
 
-const event = {
-    guests: [
-        { name: 'Tom', email: 'a@gmail.com', age: 17 },
-        { name: 'Bob', email: 'b@gmail.com', age: 18 }
-    ],
-    message: 'Welcome to the party',
-    getInvintations() {
-        return this.guests
-            .filter(({ age }) => age >= 18)
-            .map(({ name, email }) => ({
-                text: `Hi ${name}. ${this.message}`,
-                email
-            }));
-    }
+// const event = {
+//     guests: [
+//         { name: 'Tom', email: 'a@gmail.com', age: 17 },
+//         { name: 'Bob', email: 'b@gmail.com', age: 18 }
+//     ],
+//     message: 'Welcome to the party',
+//     getInvintations() {
+//         return this.guests
+//             .filter(({ age }) => age >= 18)
+//             .map(({ name, email }) => ({
+//                 text: `Hi ${name}. ${this.message}`,
+//                 email
+//             }));
+//     }
+// }
+// console.log(event.getInvintations());
+
+// const event = {
+//     guests: [
+//         { name: 'Tom', email: 'a@gmail.com', age: 17 },
+//         { name: 'Bob', email: 'b@gmail.com', age: 18 }
+//     ],
+//     message: 'Welcome to the party',
+//     getInvintations() {
+//         return this.guests
+//             .filter(({ age }) => age >= 18)
+//             .map(function ({ name, email }) {
+//                 console.log(this);
+//                 return {
+//                     text: `Hi ${name}. ${this.message}`,
+//                     email
+//                 };
+//             }, event);
+//     }
+// }
+// console.log(event.getInvintations());
+
+// //// / / / / / / //  method deposit
+
+// const wallet = {
+//     transactions: [1, 2, 89, 337, 3],
+//     getMax(){
+//         return Math.max(...this.transactions)
+//     },
+//     getMin(){
+//         return Math.min(...this.transactions)
+//     }
+// };
+// console.log(wallet.getMax());
+// console.log(wallet.getMin());
+
+
+// ////// // pseudo  arrays
+
+function argsSum() {
+    return [].reduce.call(arguments, (acc, elem) => {
+        return acc + elem;
+    }, 0);
 }
-console.log(event.getInvintations());
+
+console.log(argsSum(1, 2, 3, 4));
